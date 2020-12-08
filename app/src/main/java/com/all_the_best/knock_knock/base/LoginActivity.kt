909e7440a17b514e7e69bdf321d647f60fa74eb5
손연주 <yjoo_s@son-yeonjuui-MacBookPro.local>
2020-12-08 17:01:46 +0900

    updated week14 yjooooo

--- app/src/main/java/com/all_the_best/knock_knock/base/LoginActivity.kt
+++ app/src/main/java/com/all_the_best/knock_knock/base/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.all_the_best.knock_knock.base
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.all_the_best.knock_knock.R
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -11,9 +13,33 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val intent = Intent(this, SignupActivity::class.java)
+        val intent1 = Intent(this, SelectModeActivity::class.java)
+        val intent2 = Intent(this, SignupActivity::class.java)
+
+
         login_btn_login.setOnClickListener {
-            startActivity(intent)
+            if(login_edt_id.text.toString() == "" || login_edt_pw.text.toString() == ""){
+                Toast.makeText(this, "아이디와 비밀번호를 모두 입력해주세요.", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(this, "로그인이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                startActivity(intent1)
+            }
+
+        }
+        login_txt_signup.setOnClickListener {
+            startActivityForResult(intent2, 1)
+        }
+
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when(requestCode){
+                1 -> {
+                    login_edt_id.setText(data!!.getStringExtra("id").toString())
+                    login_edt_pw.setText(data!!.getStringExtra("pw").toString())
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/all_the_best/knock_knock/base/SignupActivity.kt
+++ app/src/main/java/com/all_the_best/knock_knock/base/SignupActivity.kt
@@ -1,8 +1,10 @@
 package com.all_the_best.knock_knock.base
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.all_the_best.knock_knock.R
 import kotlinx.android.synthetic.main.activity_signup.*
 
@@ -11,9 +13,18 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
-        val intent = Intent(this, SelectModeActivity::class.java)
+        val intent = Intent(this, LoginActivity::class.java)
         signup_btn_signup.setOnClickListener{
-            startActivity(intent)
+            val id = signup_edt_id.text.toString()
+            val pw = signup_edt_pw.text.toString()
+
+            if (id != "" && pw != ""){
+                Toast.makeText(this@SignupActivity, "반갑습니다, "+id+"님! 회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                intent.putExtra("id", id)
+                intent.putExtra("pw", pw)
+                setResult(Activity.RESULT_OK,intent)
+                finish()
+            }
         }
     }
 }
