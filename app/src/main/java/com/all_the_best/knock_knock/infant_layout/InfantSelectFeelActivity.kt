9eb9e1b180d8ceaee5eb6ecbbbb279be98a23703
손연주 <yjoo_s@son-yeonjuui-MacBookPro.local>
2020-12-10 20:30:50 +0900

    updated week15 yjooooo 아이모드 - 녹녹이 바뀐 캐릭터 적용 아이모드 - 홈화면 - 아침, 노을, 밤하늘 바뀐 화면 적용 아이모드 - 홈화면 - 버튼색 바뀐 것, 시간에 따라 바뀌게 적용 아이모드 - 캐릭터바꾸기 - 배경 시간에 따라 바뀌게 적용 아이모드 - 캐릭터바꾸기 - 캐릭터 크기 조절 (원래크기*0.86) 아이모드 - 캐릭터선택 - 캐릭터 크기 조절 (원래크기*0.89) 물결화면 변경된 것 적용 - 스플래시, 로그인, 회원가입, 부모 홈, 부모 대화/질문하기, 부모 FAQ, 부모 FAQ상세, 부모 메뉴바헤더, 아이 계정선택, 아이 캐릭터선택

--- app/src/main/java/com/all_the_best/knock_knock/infant_layout/InfantSelectFeelActivity.kt
+++ app/src/main/java/com/all_the_best/knock_knock/infant_layout/InfantSelectFeelActivity.kt
@@ -13,7 +13,6 @@ class InfantSelectFeelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_infant_select_feel)
-        val change_bg:ConstraintLayout = findViewById(R.id.infant_select_feel)
 
         val current = LocalDateTime.now()
         val formatter = DateTimeFormatter.ISO_LOCAL_TIME
@@ -21,10 +20,10 @@ class InfantSelectFeelActivity : AppCompatActivity() {
 
 
         when(formatted){
-            in "08:00:000".."13:59:999" -> change_bg.setBackgroundResource(R.drawable.infant_home_bg1)
-            in "14:00:000".."19:59:999" -> change_bg.setBackgroundResource(R.drawable.infant_home_bg2)
-            in "20:00:00".."23:59:999" -> change_bg.setBackgroundResource(R.drawable.infant_home_bg3)
-            !in "08:00:00".."23:59:999" -> change_bg.setBackgroundResource(R.drawable.infant_home_bg3)
+            in "08:00:000".."13:59:999" -> infant_select_feel.setBackgroundResource(R.drawable.infant_home_bg1)
+            in "14:00:000".."19:59:999" -> infant_select_feel.setBackgroundResource(R.drawable.infant_home_bg2)
+            in "20:00:00".."23:59:999" -> infant_select_feel.setBackgroundResource(R.drawable.infant_home_bg3)
+            !in "08:00:00".."23:59:999" -> infant_select_feel.setBackgroundResource(R.drawable.infant_home_bg3)
         }
 
         val intent = Intent(this, InfantSelectPersonActivity::class.java)
--- app/src/main/java/com/all_the_best/knock_knock/infant_layout/InfantSwitchCharacterActivity.kt
+++ app/src/main/java/com/all_the_best/knock_knock/infant_layout/InfantSwitchCharacterActivity.kt
@@ -3,13 +3,17 @@ package com.all_the_best.knock_knock.infant_layout
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.viewpager.widget.ViewPager
 import com.all_the_best.knock_knock.R
+import kotlinx.android.synthetic.main.activity_infant_home.*
 import kotlinx.android.synthetic.main.activity_infant_select_character.dots_indicator
 import kotlinx.android.synthetic.main.activity_infant_switch_character.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class InfantSwitchCharacterActivity : AppCompatActivity() {
-    private lateinit var switchViewPagerAdapter: InfantViewPagerAdapter
+    private lateinit var switchViewPagerAdapter: InfantSwitchViewPagerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_infant_switch_character)
@@ -19,7 +23,27 @@ class InfantSwitchCharacterActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        switchViewPagerAdapter = InfantViewPagerAdapter(supportFragmentManager)
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ISO_LOCAL_TIME
+        val formatted = current.format(formatter)
+
+
+        when(formatted){
+            in "08:00:000".."13:59:999" -> {
+                infant_switch_character.setBackgroundResource(R.drawable.infant_home_bg1)
+            }
+            in "14:00:000".."19:59:999" -> {
+                infant_switch_character.setBackgroundResource(R.drawable.infant_home_bg2)
+            }
+            in "20:00:00".."23:59:999" -> {
+                infant_switch_character.setBackgroundResource(R.drawable.infant_home_bg3)
+            }
+            !in "08:00:00".."23:59:999" -> {
+                infant_switch_character.setBackgroundResource(R.drawable.infant_home_bg3)
+            }
+        }
+
+        switchViewPagerAdapter = InfantSwitchViewPagerAdapter(supportFragmentManager)
 
         infant_viewpager_switch.adapter = switchViewPagerAdapter
         dots_indicator.setViewPager(infant_viewpager_switch)
