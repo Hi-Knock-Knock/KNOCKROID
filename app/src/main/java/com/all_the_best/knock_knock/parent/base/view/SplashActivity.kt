d76a6d41aca0e89b4bac7bff5ce394325750e6fb
yjooooo <thsduswn4141@naver.com>
2021-02-24 12:47:01 +0900

    Connect Firebase Realtime Database

--- app/src/main/java/com/all_the_best/knock_knock/parent/base/view/SplashActivity.kt
+++ app/src/main/java/com/all_the_best/knock_knock/parent/base/view/SplashActivity.kt
@@ -3,6 +3,7 @@ package com.all_the_best.knock_knock.parent.base.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.all_the_best.knock_knock.R
 import kotlinx.android.synthetic.main.activity_splash.*
 
@@ -15,5 +16,17 @@ class SplashActivity : AppCompatActivity() {
         img_dami.setOnClickListener{
             startActivity(intent)
         }
+
+        //setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
+    }
+    private fun setWindowFlag(bits: Int, on: Boolean) {
+        val win = window ?: return
+        val winParams = win.attributes
+        if (on) {
+            winParams.flags = winParams.flags or bits
+        } else {
+            winParams.flags = winParams.flags and bits.inv()
+        }
+        win.attributes = winParams
     }
 }
--- app/src/main/java/com/all_the_best/knock_knock/parent/setting/view/ParentAlarmSettingActivity.kt
+++ app/src/main/java/com/all_the_best/knock_knock/parent/setting/view/ParentAlarmSettingActivity.kt
@@ -2,11 +2,24 @@ package com.all_the_best.knock_knock.parent.setting.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.all_the_best.knock_knock.R
 
 class ParentAlarmSettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_parent_alarm_setting)
+
+        //setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true)
+    }
+    private fun setWindowFlag(bits: Int, on: Boolean) {
+        val win = window ?: return
+        val winParams = win.attributes
+        if (on) {
+            winParams.flags = winParams.flags or bits
+        } else {
+            winParams.flags = winParams.flags and bits.inv()
+        }
+        win.attributes = winParams
     }
 }
