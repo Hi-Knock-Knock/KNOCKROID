ed3f52fc3ed232511e11734be2d243b776c38351
Yunjoo Son <thsduswn4141@naver.com>
2021-03-18 03:09:38 +0900

    #72 MyScrap : 버튼 연결(뒤로가기 버튼 연결, FAQ 바로가기 버튼 연결

--- app/src/main/java/com/all_the_best/knock_knock/parent/mypage/view/ParentMyScrapActivity.kt
+++ app/src/main/java/com/all_the_best/knock_knock/parent/mypage/view/ParentMyScrapActivity.kt
@@ -1,19 +1,37 @@
 package com.all_the_best.knock_knock.parent.mypage.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.all_the_best.knock_knock.R
 import com.all_the_best.knock_knock.databinding.ActivityParentMyScrapBinding
+import com.all_the_best.knock_knock.parent.home.view.ParentHomeActivity
 import com.all_the_best.knock_knock.util.StatusBarUtil
 
 class ParentMyScrapActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityParentMyScrapBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         StatusBarUtil.setStatusBar(this, resources.getColor(R.color.white_status_bar, null))
-
-        val binding: ActivityParentMyScrapBinding = DataBindingUtil.setContentView(this, R.layout.activity_parent_my_scrap)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_parent_my_scrap)
         binding.txtGoFaq = "FAQ 바로가기"
+        setOnClickListenerForGoBack()
+        setOnClickListenerForGoFaq()
+    }
+
+    private fun setOnClickListenerForGoBack() {
+        binding.parentMyScrapBtnBack.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    private fun setOnClickListenerForGoFaq() {
+        binding.parentMyScrapTxtGoFaq.setOnClickListener {
+            val intent = Intent(this, ParentHomeActivity::class.java)
+            intent.putExtra("goFaq", true)
+            startActivity(intent)
+            finish()
+        }
     }
 }
