4b5b3bb3b8ed7b1356bb1f64e65846cf937fe0ee
yoounseules <yoonseules@naver.com>
2021-03-18 04:40:56 +0900

    infant_deco,bg img 추가 및 xml 수정, deco_button 수정

--- app/src/main/java/com/all_the_best/knock_knock/infant/deco/view/InfantDecoActivity.kt
+++ app/src/main/java/com/all_the_best/knock_knock/infant/deco/view/InfantDecoActivity.kt
@@ -11,40 +11,73 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class InfantDecoActivity : AppCompatActivity() {
-
        override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_infant_deco)
+           super.onCreate(savedInstanceState)
+           setContentView(R.layout.activity_infant_deco)
+           val current = LocalDateTime.now()
+           val formatter = DateTimeFormatter.ISO_LOCAL_TIME
+           val formatted = current.format(formatter)
+           var bg_select : Int? = null
 
-        val current = LocalDateTime.now()
-        val formatter = DateTimeFormatter.ISO_LOCAL_TIME
-        val formatted = current.format(formatter)
+           deco_item_bg1.setOnClickListener {
+               bg_select = 1
+               deco_item_bg1.setBackgroundResource(R.drawable.bg_deco_select_item)
+               deco_item_bg2.setBackgroundResource(R.drawable.bg_deco_unselect_item)
+               deco_item_bg3.setBackgroundResource(R.drawable.bg_deco_unselect_item)
+           }
+           deco_item_bg2.setOnClickListener {
+               bg_select = 2
+               deco_item_bg1.setBackgroundResource(R.drawable.bg_deco_unselect_item)
+               deco_item_bg2.setBackgroundResource(R.drawable.bg_deco_select_item)
+               deco_item_bg3.setBackgroundResource(R.drawable.bg_deco_unselect_item)
+           }
+           deco_item_bg3.setOnClickListener {
+               bg_select = 3
+               deco_item_bg1.setBackgroundResource(R.drawable.bg_deco_unselect_item)
+               deco_item_bg2.setBackgroundResource(R.drawable.bg_deco_unselect_item)
+               deco_item_bg3.setBackgroundResource(R.drawable.bg_deco_select_item)
+           }
 
-        when(formatted) {
-            in "08:00:000".."13:59:999" -> {
-                infant_deco.setBackgroundResource(R.drawable.img_infant_deco_morning_bg)
-                window.statusBarColor = Color.parseColor("#57DDFF")
-            }
-            in "14:00:000".."19:59:999" -> {
-                infant_deco.setBackgroundResource(R.drawable.img_infant_deco_after_bg)
-                window.statusBarColor = Color.parseColor("#FF6471")
-            }
-            in "20:00:00".."23:59:999" -> {
-                infant_deco.setBackgroundResource(R.drawable.img_infant_deco_night_bg)
-                window.statusBarColor = Color.parseColor("#0F0E15")
-            }
-            !in "08:00:00".."23:59:999" -> {
-                infant_deco.setBackgroundResource(R.drawable.img_infant_deco_night_bg)
-                window.statusBarColor = Color.parseColor("#0F0E15")
-            }
-        }
+           when(formatted) {
+               in "08:00:000".."13:59:999" -> {
+                   window.statusBarColor = Color.parseColor("#57DDFF")
+                   when(bg_select){
+                       1 -> {infant_deco.setBackgroundResource(R.drawable.img_infant_deco_morning_bg)}
+                       2 -> {infant_deco.setBackgroundResource(R.drawable.img_infant_deco_bg_flower1)}
+                       3 -> {infant_deco.setBackgroundResource(R.drawable.img_infant_deco_bg_sea1)}
+                   }
+               }
+               in "14:00:000".."19:59:999" -> {
+                   window.statusBarColor = Color.parseColor("#FF6471")
+                   when(bg_select){
+                       1 -> {infant_deco.setBackgroundResource(R.drawable.img_infant_deco_after_bg)}
+                       2 -> {infant_deco.setBackgroundResource(R.drawable.img_infant_deco_bg_flower2)}
+                       3 -> {infant_deco.setBackgroundResource(R.drawable.img_infant_deco_bg_sea2)}
+                   }
+               }
+               in "20:00:00".."23:59:999" -> {
+                   window.statusBarColor = Color.parseColor("#0F0E15")
+                   when(bg_select){
+                       1 -> {infant_deco.setBackgroundResource(R.drawable.img_infant_deco_night_bg)}
+                       2 -> {infant_deco.setBackgroundResource(R.drawable.img_infant_deco_bg_flower3)}
+                       3 -> {infant_deco.setBackgroundResource(R.drawable.img_infant_deco_bg_sea3)}
+                   }
+               }
+               !in "08:00:00".."23:59:999" -> {
+                   window.statusBarColor = Color.parseColor("#0F0E15")
+                   when(bg_select){
+                       1 -> {infant_deco.setBackgroundResource(R.drawable.img_infant_deco_night_bg)}
+                       2 -> {infant_deco.setBackgroundResource(R.drawable.img_infant_deco_bg_flower3)}
+                       3 -> {infant_deco.setBackgroundResource(R.drawable.img_infant_deco_bg_sea3)}
+                   }
+               }
+           }
 
 
-        val intent = Intent(this, InfantHomeActivity::class.java)
-        infant_icon_deco_out1.setOnClickListener{
-            startActivity(intent)
-            overridePendingTransition(0, 0)
-        }
-
-        }
- }
+           val intent = Intent(this, InfantHomeActivity::class.java)
+           infant_icon_deco_out1.setOnClickListener{
+               startActivity(intent)
+               overridePendingTransition(0, 0)
+           }
+       }
+}
