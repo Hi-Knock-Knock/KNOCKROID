65acabfa8fc57e7bdcba7b3895523777a7a00ec6
Yunjoo Son <thsduswn4141@naver.com>
2021-03-18 02:54:21 +0900

    #70 EditProfile : 버튼 연결(뒤로가기, 프로필 수정)

--- app/src/main/java/com/all_the_best/knock_knock/parent/mypage/view/ParentEditProfileActivity.kt
+++ app/src/main/java/com/all_the_best/knock_knock/parent/mypage/view/ParentEditProfileActivity.kt
@@ -2,19 +2,35 @@ package com.all_the_best.knock_knock.parent.mypage.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.all_the_best.knock_knock.R
 import com.all_the_best.knock_knock.databinding.ActivityParentEditProfileBinding
 import com.all_the_best.knock_knock.util.StatusBarUtil
 
 class ParentEditProfileActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityParentEditProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         StatusBarUtil.setStatusBar(this, resources.getColor(R.color.white_status_bar, null))
 
-        val binding: ActivityParentEditProfileBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_parent_edit_profile)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_parent_edit_profile)
         binding.txtOk = "확인"
+        setOnClickListenerForBtnGoBack()
+        setOnClickListenerForBtnOk()
+    }
+
+    private fun setOnClickListenerForBtnGoBack() {
+        binding.parentEditProfileBtnBack.setOnClickListener {
+            onBackPressed()
+        }
+    }
+
+    private fun setOnClickListenerForBtnOk() {
+        binding.parentEditProfileTxtOk.setOnClickListener {
+            Toast.makeText(this, "별명이 변경되었습니다.", Toast.LENGTH_SHORT).show()
+            //프로필 별명 수정 코드 작성하기
+        }
     }
 }
