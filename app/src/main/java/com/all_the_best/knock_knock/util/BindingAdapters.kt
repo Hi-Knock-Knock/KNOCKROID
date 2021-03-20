ff13b2141be7b77efbb14feed6fa03d7a49ec55e
Yunjoo Son <thsduswn4141@naver.com>
2021-03-20 18:46:42 +0900

    #65 Talk : Dialog 디자인 수정 후 띄우기

--- app/src/main/java/com/all_the_best/knock_knock/util/BindingAdapters.kt
+++ app/src/main/java/com/all_the_best/knock_knock/util/BindingAdapters.kt
@@ -1,16 +1,21 @@
 package com.all_the_best.knock_knock.util
 
+import android.annotation.SuppressLint
 import android.text.Spannable
 import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.text.style.UnderlineSpan
+import android.util.Log
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.all_the_best.knock_knock.R
 
 object BindingAdapters {
     @BindingAdapter("setUnderLine")
     @JvmStatic
-    fun setUnderLine(textView: TextView, delete: String) {
-        val text = delete
+    fun setUnderLine(textView: TextView, txt: String) {
+        Log.d("dialog",txt)
+        val text = txt
         val spannableString = SpannableStringBuilder(text)
         spannableString.setSpan(
             UnderlineSpan(),
@@ -26,4 +31,19 @@ object BindingAdapters {
     fun setTitleFromHome(textView: TextView, nickname: String) {
         textView.text = nickname + "의 오늘"
     }
+
+    @BindingAdapter("help_dialog:txt", "help_dialog:start", "help_dialog:end")
+    @JvmStatic
+    fun setBlueColor(textView: TextView, txt: String, start: Int, end: Int) {
+        val text = txt
+        val spannableString = SpannableStringBuilder(text)
+        spannableString.setSpan(
+            ForegroundColorSpan(textView.context.getColor(R.color.help_dialog_blue)),
+            start,
+            end,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        textView.text = spannableString
+    }
+
 }
