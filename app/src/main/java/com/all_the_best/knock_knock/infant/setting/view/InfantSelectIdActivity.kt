f69806a59dc8b3711ff5d9962727fbab547545c2
Yunjoo Son <thsduswn4141@naver.com>
2021-03-20 19:14:12 +0900

    #89 Talk : 필요없는 뷰 삭제

--- app/src/main/java/com/all_the_best/knock_knock/infant/setting/view/InfantSelectIdActivity.kt
+++ app/src/main/java/com/all_the_best/knock_knock/infant/setting/view/InfantSelectIdActivity.kt
@@ -2,12 +2,9 @@ package com.all_the_best.knock_knock.infant.setting.view
 
 import android.content.Intent
 import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.all_the_best.knock_knock.R
-import com.all_the_best.knock_knock.parent.talk.view.ParentRealTalkActivity
 import kotlinx.android.synthetic.main.activity_infant_select_id.*
 
 
@@ -23,12 +20,6 @@ class InfantSelectIdActivity : AppCompatActivity() {
         val intent = Intent(this, InfantSelectCharacterActivity::class.java)
         button1.setOnClickListener{
             startActivity(intent)
-            overridePendingTransition(0, 0)
-        }
-
-        val intent2 = Intent(this, ParentRealTalkActivity::class.java)
-        button2.setOnClickListener {
-            startActivity(intent2)
             overridePendingTransition(0, 0)
         }
     }
