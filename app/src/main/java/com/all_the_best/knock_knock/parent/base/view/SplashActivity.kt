176efea0b580c8c800136aa623b84f7b1627dc46
Yunjoo Son <thsduswn4141@naver.com>
2021-04-07 03:14:03 +0900

    110 [FEAT] : Splash Lottie 적용

--- app/src/main/java/com/all_the_best/knock_knock/parent/base/view/SplashActivity.kt
+++ app/src/main/java/com/all_the_best/knock_knock/parent/base/view/SplashActivity.kt
@@ -1,28 +1,50 @@
 package com.all_the_best.knock_knock.parent.base.view
 
+import android.animation.Animator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.provider.ContactsContract
+import android.util.Log
+import androidx.databinding.DataBindingUtil
 import com.all_the_best.knock_knock.R
+import com.all_the_best.knock_knock.databinding.ActivitySplashBinding
 import com.all_the_best.knock_knock.util.StatusBarUtil
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var splashBinding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        
+        splashBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
         StatusBarUtil.setStatusBar(this, resources.getColor(R.color.light_blue_status_bar, null))
 
-        setSplashHandler()
+        startAnimation()
+        setLottieListener()
     }
 
-    private fun setSplashHandler(){
-        Handler(Looper.getMainLooper()).postDelayed ({
-            startActivity(Intent(this, LoginActivity::class.java))
-            finish()
-        }, 2000)
+    private fun startAnimation() {
+        splashBinding.lottieSplash.playAnimation()
+    }
+
+    private fun setLottieListener() {
+        splashBinding.lottieSplash.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator?) {
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+                finish()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+
+        })
     }
 }
