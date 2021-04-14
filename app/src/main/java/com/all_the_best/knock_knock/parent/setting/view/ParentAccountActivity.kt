48124f02eb408191f7c537745665f7e3a1f14452
Yunjoo Son <thsduswn4141@naver.com>
2021-04-14 21:38:37 +0900

    #131 All : 구분선 높이 수정, 백버튼 구현

--- app/src/main/java/com/all_the_best/knock_knock/parent/setting/view/ParentAccountActivity.kt
+++ app/src/main/java/com/all_the_best/knock_knock/parent/setting/view/ParentAccountActivity.kt
@@ -2,14 +2,23 @@ package com.all_the_best.knock_knock.parent.setting.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.all_the_best.knock_knock.R
+import com.all_the_best.knock_knock.databinding.ActivityParentAccountBinding
 import com.all_the_best.knock_knock.util.StatusBarUtil
 
 class ParentAccountActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityParentAccountBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_parent_account)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_parent_account)
         StatusBarUtil.setStatusBar(this, resources.getColor(R.color.white_status_bar, null))
+        setOnBackBtnClick()
+    }
+
+    private fun setOnBackBtnClick() {
+        binding.parentAccountBtnBack.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
